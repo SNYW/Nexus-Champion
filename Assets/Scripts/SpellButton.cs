a318bbc52cad11267b6e566de31e536c1873fa9b
Sonny Williams <sonnywilliams@hotmail.co.uk>
2024-02-15 21:22:37 +0000

    Spell Cooldown

--- Assets/Scripts/SpellButton.cs
+++ Assets/Scripts/SpellButton.cs
@@ -4,21 +4,29 @@ using System.Collections.Generic;
 using Spells;
 using SystemEvents;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class SpellButton : MonoBehaviour
 {
     public KeyCode hotkey;
     public Spell spell;
-
+    public Image cooldownIndicator;
     public bool selected;
+
+    private float currentCooldown;
 
     private void Start()
     {
         selected = false;
+        currentCooldown = 0;
     }
 
     void Update()
     {
+        if(spell != null)
+            ManageCooldown();
+        if (currentCooldown > 0) return;
+        
         if (Input.GetKeyDown(hotkey))
         {
             selected = true;
@@ -35,6 +43,13 @@ public class SpellButton : MonoBehaviour
         {
             selected = false;
             SystemEventManager.RaiseEvent(SystemEventManager.SystemEventType.SpellCast, spell);
+            currentCooldown = spell.coolDown;
         }
+    }
+
+    private void ManageCooldown()
+    {
+        currentCooldown = Mathf.Clamp(currentCooldown - Time.deltaTime, 0 , float.MaxValue);
+        cooldownIndicator.fillAmount = currentCooldown / spell.coolDown;
     }
 }
